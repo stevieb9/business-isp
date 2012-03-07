3010bd85b2b9e39cb189abe8bbf6bb3558917954
steveb <steveb@cpan.org>
2012-03-07 18:45:27 -0500

    -0.13_02 added ISP.pm

---
+++ lib/Business/ISP/ISP.pm
@@ -0,0 +1,34 @@
+package Business::ISP::ISP;
+
+use strict;
+use warnings;
+no warnings 'redefine';
+
+our $VERSION = '0.13_02';
+
+=head1 NAME
+
+Business::ISP:: - An *SP billing/client accounting system.
+
+=head1 DESCRIPTION
+
+This is just a test upload so I can figure out some particular intricacies of
+CPAN :)
+
+=head1 AUTHOR
+
+Steve Bertrand, C<< <steveb at cpan.org> >>
+
+=head1 COPYRIGHT
+
+Copyright 2012 Steve Bertrand, all rights reserved.
+
+=head1 LICENSE
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+
+=cut
+
+1;
